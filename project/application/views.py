5f070ca159d4e924c1a90a42fbc915e0b44bdeab
soxklsl <saguseva6@gmail.com>
2025-06-16 03:01:45 +0300

    new html

--- project/application/views.py
+++ project/application/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, "main.html")
+
+def ent_reg(request):
+    return render(request, "ent_reg.html")
+
+def registration(request):
+    return render(request, "registration.html")
--- project/main/urls.py
+++ project/main/urls.py
@@ -4,5 +4,7 @@ from application import views
 
 urlpatterns = [
     path('', views.main),
+    path('ent_reg', views.ent_reg),
+    path('registration/', views.registration),
     path('admin/', admin.site.urls)
 ]
