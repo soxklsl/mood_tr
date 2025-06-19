3e8f22b9a5e265afdd9f7fa70e603155b707907e
soxklsl <saguseva6@gmail.com>
2025-06-19 22:18:54 +0300

    transition

--- project/main/urls.py
+++ project/main/urls.py
@@ -5,7 +5,7 @@ from application import views
 urlpatterns = [
     path('main', views.main),
     path('', views.ent_reg),
-    path('registration/', views.registration),
-    path('entrance/', views.entrance),
+    path('registration/', views.registration, name="reg"),
+    path('entrance/', views.entrance, name="ent"),
     path('admin/', admin.site.urls)
 ]
