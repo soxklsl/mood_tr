1dbcb4586d08adc8c6c6e8c69505bbdabaeeecab
soxklsl <saguseva6@gmail.com>
2025-06-21 00:27:45 +0300

    add additional form password

--- project/application/views.py
+++ project/application/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Users
 
